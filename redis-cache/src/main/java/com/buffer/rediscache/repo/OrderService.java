2329eecf88b34aa879ad20297b29366b0b56eb2e
Octavio Sánchez Huerta <osanchezhuerta@gmail.com>
2024-10-22 12:12:23 -0600

    project

---
+++ redis-cache/src/main/java/com/buffer/rediscache/repo/OrderService.java
@@ -0,0 +1,44 @@
+package com.buffer.rediscache.repo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.stereotype.Service;
+
+import com.buffer.rediscache.OrderNotFoundException;
+
+
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public Order save(Order order) {
+		repository.save(order);
+		return order;
+	}
+
+	@Cacheable(value = "order")
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	@Cacheable(value = "order", key = "#id", condition="#id>=10")
+	public Order findOrderById(int id) {
+		return repository.findById(id).orElseThrow(() -> new OrderNotFoundException("Not found"));
+	}
+
+	@Caching(
+			  evict = {@CacheEvict(value = "order", allEntries = true), @CacheEvict(value="order", key="#id")
+			}) 
+	public String deleteOrder(int id) {
+		repository.deleteById(id);
+		return "Order deleted successfully!";
+	}
+
+}
